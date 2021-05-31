7268d9196fe4ccd24c577e4751f46e42574ba5b2
Emil Jan <anoldwebsite@gmail.com>
2021-05-31 13:22:08 +0100

    footer with responsiveness added to the website

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { ThemeProvider } from '@material-ui/styles'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from '../components/ui/header';
+import Footer from '../components/ui/footer';
+import theme from './ui/theme';
+
+function App() {
+
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [value, setValue] = useState(0);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Header value={value} setValue={setValue} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex} />
+        <Switch>
+          <Route exact path="/" component={() => <div style={{ height: "2000px" }}>Home</div>} />
+          <Route exact path="/services" component={() => <div>Services</div>} />
+          <Route exact path="/customsoftware" component={() => <div>Custom Software</div>} />
+          <Route exact path="/mobileapps" component={() => <div>Mobile Apps</div>} />
+          <Route exact path="/websites" component={() => <div>Websites</div>} />
+          <Route exact path="/revolution" component={() => <div>The Revolution</div>} />
+          <Route exact path="/about" component={() => <div>About Us</div>} />
+          <Route exact path="/contact" component={() => <div>Contact Us</div>} />
+          <Route exact path="/estimate" component={() => <div>Get a Free Estimate</div>} />
+        </Switch>
+        <Footer value={value} setValue={setValue} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex} />
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
